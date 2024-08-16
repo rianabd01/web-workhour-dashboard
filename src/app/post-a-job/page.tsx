9602273d096fa296a7784b8e15bd74c03bddd89f
rianabd01 <rianwahyuab@gmail.com>
2024-08-16 10:08:26 +0700

    add: post a job page

---
+++ src/app/post-a-job/page.tsx
@@ -0,0 +1,23 @@
+import { jobFormSchema } from "@/lib/form-schema";
+import React, { FC } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+interface PostAJobPage {}
+
+const PostAJob: FC<PostAJobPage> = ({}) => {
+  const form = useForm<z.infer<typeof jobFormSchema>>({
+    resolver: zodResolver(jobFormSchema),
+    defaultValues: {
+      requiredSkills: [],
+    },
+  });
+
+  const onSubmit = (val: z.infer<typeof jobFormSchema>) => {
+    console.log(val);
+  };
+  return <div>Post Job</div>;
+};
+
+export default PostAJob;
