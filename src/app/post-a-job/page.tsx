ad74dd66dd5cef706bc6943f7ef95deaf9b6ca62
rianabd01 <rianwahyuab@gmail.com>
2024-08-17 09:25:16 +0700

    initial commit

--- src/app/post-a-job/page.tsx
+++ src/app/post-a-job/page.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { jobFormSchema } from "@/lib/form-schema";
 import React, { FC } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowLeft } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 interface PostAJobPage {}
 
@@ -17,7 +20,23 @@ const PostAJob: FC<PostAJobPage> = ({}) => {
   const onSubmit = (val: z.infer<typeof jobFormSchema>) => {
     console.log(val);
   };
-  return <div>Post Job</div>;
+  return (
+    <div>
+      <div className="inline-flex items-center gap-5 hover:text-primary cursor-pointer">
+        <ArrowLeft className="w-7 h-7" />
+        <div className="text-lg font-semibold">Post Job</div>
+      </div>
+      <div className="my-5">
+        <div className="text-lg font-semibold">Basic information</div>
+        <div className="text-gray-400">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, harum?
+        </div>
+      </div>
+
+      <Separator />
+    </div>
+  );
 };
 
 export default PostAJob;
